b9031133e67bd849f106875c4df8964e3d4bcc2f
nielleeeee <jandnllplz@gmail.com>
2026-03-20 18:16:58 +0800

    chore: added font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,31 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const akrobat = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Akrobat-Regular.woff",
+      weight: "500", // Standard regular for Akrobat is often 500
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Akrobat-SemiBold.woff",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Akrobat-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Akrobat-ExtraBold.woff",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+  variable: "--font-akrobat",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${akrobat.variable} font-sans antialiased`}
       >
         {children}
       </body>
