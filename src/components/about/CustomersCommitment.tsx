9c342f2d8020c209bb768c07950090b07b2a6c3f
Nielleeeee <jandnllplz@gmail.com>
2026-03-25 04:10:28 +0800

    chore: added undefined check

--- src/components/about/CustomersCommitment.tsx
+++ src/components/about/CustomersCommitment.tsx
@@ -1,18 +1,27 @@
 import { PageAboutQueryResult } from "../../../sanity.types";
 
-type CustomersCommitment = NonNullable<PageAboutQueryResult>["customersCommitment"];
+type CustomersCommitment =
+  NonNullable<PageAboutQueryResult>["customersCommitment"];
 
-export const CustomersCommitment = ({ data }: { data: CustomersCommitment }) => {
-    return (
-        <section id="customers-commitment">
-            <div>
-                <h3>{data?.targetCustomers?.title || "Target Customers"}</h3>
-                <p>{data?.targetCustomers?.description}</p>
-            </div>
-            <div>
-                <h3>{data?.ourCommitment?.title || "Our Commitment"}</h3>
-                <p>{data?.ourCommitment?.description}</p>
-            </div>
-        </section>
-    );
+export const CustomersCommitment = ({
+  data,
+}: {
+  data: CustomersCommitment;
+}) => {
+  if (!data) return null;
+
+  const { graphic, targetCustomers, ourCommitment } = data;
+
+  return (
+    <section id="customers-commitment">
+      <div>
+        <h3>{targetCustomers?.title || "Target Customers"}</h3>
+        <p>{targetCustomers?.description}</p>
+      </div>
+      <div>
+        <h3>{ourCommitment?.title || "Our Commitment"}</h3>
+        <p>{ourCommitment?.description}</p>
+      </div>
+    </section>
+  );
 };
