6b803bd1d1e0d93d51003b5ebfee356a0c966cbd
Nielleeeee <jandnllplz@gmail.com>
2026-03-25 04:10:08 +0800

    chore: added undefined check

--- src/components/about/CompanyInformation.tsx
+++ src/components/about/CompanyInformation.tsx
@@ -3,17 +3,21 @@ import { PageAboutQueryResult } from "../../../sanity.types";
 type companyInformation = NonNullable<PageAboutQueryResult>["companyInformation"];
 
 export const CompanyInformation = ({ data }: { data: companyInformation }) => {
+    if (!data) return null;
+
+    const { companyName, brandName, industry, coreFocus } = data;
+
     return (
         <section id="company-information">
-            <h3>{data?.companyName || "Company Information"}</h3>
+            <h3>{companyName || "Company Information"}</h3>
             <p>
-                <strong>Brand:</strong> {data?.brandName}
+                <strong>Brand:</strong> {brandName}
             </p>
             <p>
-                <strong>Industry:</strong> {data?.industry}
+                <strong>Industry:</strong> {industry}
             </p>
             <p>
-                <strong>Focus:</strong> {data?.coreFocus}
+                <strong>Focus:</strong> {coreFocus}
             </p>
         </section>
     );
